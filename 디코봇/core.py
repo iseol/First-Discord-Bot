e376d19698bfa9f3d4eceb7d17153149322ea614
iseol421 <iseol421@gmail.com>
2020-08-29 11:11:04 +0900

    fdfdfdfdfdf

--- 디코봇/core.py
+++ 디코봇/core.py
@@ -1,22 +1,51 @@
 import discord
 import random
-from discord.ext import commands
+import os
+from discord.ext import commands, tasks
+from itertools import cycle
+
+token = 'NzIyNjkwOTM0NjcxODAyNDQ5.XumwYQ.3UIiSSLsldV8kOo0CAM6Aemx6rY'
+
+EMBED_URL = "https://cdn.discordapp.com/attachments/697450184875049010/747816940625723502/Screenshot_20200825-224051_Love_LiveAS.jpg"
 
 client = commands.Bot(command_prefix='.')
+status_url = 'https://www.youtube.com/watch?v=1cb8klW1hxs'
+status = cycle(['와 샌증', '뀽뀽'])
 
+client.remove_command('help') # 기존 help 명령어 제거
 
 @client.event
 async def on_ready():
+    change_status.start()
     print('Bot is ready.')
 
+# @client.event
+# async def on_command_error(ctx, error):
+    # if isinstance(error, commands.MissingRequiredArgument):
+        # await ctx.send('제대로 써 빙구야')
+
+# @client.event
+# async def on_command_error(ctx, error):
+    # if isinstance(error, commands.CommandNotFound):
+        # await ctx.send('잘못된 명령어임')
+
+@tasks.loop(seconds=5)
+async def change_status(): # start()는 필수
+    await client.change_presence(activity=discord.Streaming(name=next(status), url=status_url))
 
 @client.command()
 async def 핑(ctx):
     await ctx.send(f'퐁! {round(client.latency * 1000)}ms') # 핑을 표시
 
 @client.command()
-async def 삭제(ctx, amount=5): # ".삭제" 까지 지움
+async def 삭제(ctx, amount : int): # ".삭제" 까지 지움
     await ctx.channel.purge(limit=amount)
+
+@삭제.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('삭제 뒤에 숫자를 써야지 뭐하냐')
+
 
 @client.command()
 async def 킥(ctx, member : discord.Member, *, reason=None):
@@ -24,10 +53,52 @@ async def 킥(ctx, member : discord.Member, *, reason=None):
 
 @client.command()
 async def 밴(ctx, member : discord.Member, *, reason=None):
-    await member.ban(reason=reason)  
+    await member.ban(reason=reason)
 
+@client.command()
+async def 언밴(ctx, *, member): # member 은 '유저#0001' 이런 형식
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#');
 
-@client.command(aliases=['8ball', 'test'])
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+            return
+
+@client.command()
+async def displayembed(ctx):
+    embed = discord.Embed(
+    title = 'Title',
+    description = 'This is a description.',
+    color = discord.Color.blue()
+    )
+    embed.set_footer(text='This is a footer.')
+    embed.set_image(url=EMBED_URL)
+    embed.set_thumbnail(url=EMBED_URL)
+    embed.set_author(name='Author Name',
+    icon_url=EMBED_URL)
+    embed.add_field(name='Field Name', value='Field Value', inline=False)
+    embed.add_field(name='Field Name', value='Field Value', inline=True)
+    embed.add_field(name='Field Name', value='Field Value', inline=True)
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def help(ctx): # 개인 메세지
+    author = ctx.message.author
+
+    embed = discord.Embed(
+    color = discord.Color(0x7289da) # Hex Color
+    )
+    embed.set_author(name='Help')
+    embed.add_field(name='.ping', value='Returns Pong!', inline=False)
+
+    await author.send(embed=embed)
+
+@client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
     responses = ['It is certain.',
                 'It is decidedly so.',
@@ -51,7 +122,22 @@ async def _8ball(ctx, *, question):
                 'Very doubtful.']
     await ctx.send(f'Question : {question}\nAnswer: {random.choice(responses)}')
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
 
 
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("NzIyNjkwOTM0NjcxODAyNDQ5.Xumwvw.EHxeeTV1tWH20pIlFyLkpIO-ovQ")
+client.run(token)
